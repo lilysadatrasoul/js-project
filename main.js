ae067c4ac1eed7d122e6af053d53ec8944fbebdd
Lily Sadat <l.sadatrasoul@gmail.com>
2022-02-17 18:41:11 +0330

    disable div

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const player_x = `X`;
 const player_o = `O`;
-var turn = 0;
+let turn = 0;
+let finished = false;
 
 
 
 
 
-function getEntry(player, index) {
+function getEntry(player) {
     let tile1 = document.getElementById("tile-1").innerHTML;
     let tile2 = document.getElementById("tile-2").innerHTML;
     let tile3 = document.getElementById("tile-3").innerHTML;
@@ -18,66 +19,74 @@ function getEntry(player, index) {
     let tile9 = document.getElementById("tile-9").innerHTML;
 
 
-
-    if (((tile1 == player) && (tile2 == player) && (tile3 == player)) ||             // 1
+    if (((tile1 == player) && (tile2 == player) && (tile3 == player)) ||                            // 1
         (tile1 == player) && (tile4 == player) && (tile7 == player) ||
         (tile1 == player) && (tile5 == player) && (tile9 == player)) {
-        let alertWin = document.getElementById(`which-player-win`);
-        alertWin.innerHTML = "Winner is " + player;
-    } else if (((tile2 == player) && (tile1 == player) && (tile3 == player)) ||        // 2
+            return true;
+    } else if (((tile2 == player) && (tile1 == player) && (tile3 == player)) ||                       // 2
         (tile2 == player) && (tile5 == player) && (tile8 == player)) {
-        let alertWin = document.getElementById(`which-player-win`);
-        alertWin.innerHTML = "Winner is " + player;
-    } else if (((tile3 == player) && (tile1 == player) && (tile2 == player)) ||         // 3
-        (tile3 == player) && (tile6 == player) && (tile9 == player)||
+            return true;
+    } else if (((tile3 == player) && (tile1 == player) && (tile2 == player)) ||                       // 3
+        (tile3 == player) && (tile6 == player) && (tile9 == player) ||
         (tile3 == player) && (tile5 == player) && (tile7 == player)) {
-        let alertWin = document.getElementById(`which-player-win`);
-        alertWin.innerHTML = "Winner is " + player;
-    } else if (((tile4 == player) && (tile1 == player) && (tile7 == player)) ||         // 4
+            return true;
+    } else if (((tile4 == player) && (tile1 == player) && (tile7 == player)) ||                       // 4
         (tile4 == player) && (tile5 == player) && (tile6 == player)) {
-        let alertWin = document.getElementById(`which-player-win`);
-        alertWin.innerHTML = "Winner is " + player;
-    } else if (((tile5 == player) && (tile2 == player) && (tile8 == player)) ||          // 5 
+            return true;
+    } else if (((tile5 == player) && (tile2 == player) && (tile8 == player)) ||                      // 5 
         (tile5 == player) && (tile4 == player) && (tile6 == player) ||
         (tile5 == player) && (tile3 == player) && (tile7 == player) ||
         (tile5 == player) && (tile1 == player) && (tile9 == player)) {
-        let alertWin = document.getElementById(`which-player-win`);
-        alertWin.innerHTML = "Winner is " + player;
-    } else if (((tile6 == player) && (tile3 == player) && (tile9 == player)) ||           // 6
+            return true;
+    } else if (((tile6 == player) && (tile3 == player) && (tile9 == player)) ||                      // 6
         (tile6 == player) && (tile4 == player) && (tile5 == player)) {
-        let alertWin = document.getElementById(`which-player-win`);
-        alertWin.innerHTML = "Winner is " + player;
-    } else if (((tile7 == player) && (tile1 == player) && (tile4 == player)) ||           // 7 
+            return true;
+    } else if (((tile7 == player) && (tile1 == player) && (tile4 == player)) ||                     // 7 
         (tile7 == player) && (tile5 == player) && (tile3 == player) ||
         (tile7 == player) && (tile8 == player) && (tile9 == player)) {
-        let alertWin = document.getElementById(`which-player-win`);
-        alertWin.innerHTML = "Winner is " + player;
-    } else if (((tile8 == player) && (tile2 == player) && (tile5 == player)) ||                    // 8
+            return true;
+    } else if (((tile8 == player) && (tile2 == player) && (tile5 == player)) ||                      // 8
         (tile8 == player) && (tile9 == player) && (tile7 == player)) {
-        let alertWin = document.getElementById(`which-player-win`);
-        alertWin.innerHTML = "Winner is " + player;
+            return true;
     } else if (((tile9 == player) && (tile6 == player) && (tile3 == player)) ||                       // 9
         (tile9 == player) && (tile8 == player) && (tile7 == player) ||
         (tile9 == player) && (tile5 == player) && (tile1 == player)) {
-        let alertWin = document.getElementById(`which-player-win`);
-        alertWin.innerHTML = "Winner is " + player;
+            return true;
     }
+
+    return false;
 }
 
 
 var tileElements = document.getElementsByClassName('tile');
 for (var i = 0; i < tileElements.length; i++) {
     tileElements[i].addEventListener('click', function () {
-        if (this.innerHTML == '') {
-            if (turn == 0) {
-                this.innerHTML = player_x;
-                getEntry(player_x);
-                turn = 1;
-            } else {
-                this.innerHTML = player_o;
-                getEntry(player_o);
-                turn = 0;
+        if (finished==false) {
+            if (this.innerHTML == '') {
+                if (turn == 0) {
+                    this.innerHTML = player_x;
+                    if(getEntry(player_x)){
+                        let alertWin = document.getElementById(`which-player-win`);
+                        alertWin.innerHTML = "Winner is " + " " + player_x;
+                        finished = true;
+                    }
+                    turn = 1;
+
+                } else {
+                    this.innerHTML = player_o;
+                    if(getEntry(player_o)){
+                        let alertWin = document.getElementById(`which-player-win`);
+                        alertWin.innerHTML = "Winner is " + " " + player_o;
+                        finished = true;
+                    }
+                    turn = 0;
+                }
             }
         }
     });
 }
+if()
+function resetboard(){  
+    document.getElementById("boardId").reset();  
+  } 
+  
