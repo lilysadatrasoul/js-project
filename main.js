42045138abf075682d06cfd33b198444068485e2
Lily Sadat <l.sadatrasoul@gmail.com>
2022-02-19 23:10:32 +0330

    final changing

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ const player_x = `X`;
 const player_o = `O`;
 let turn = 0;
 let finished = false;
-
-
-
-
+let score_x = 0;
+let score_o = 0;
 
 function getEntry(player) {
     let tile1 = document.getElementById("tile-1").innerHTML;
@@ -19,67 +17,71 @@ function getEntry(player) {
     let tile9 = document.getElementById("tile-9").innerHTML;
 
 
-    if (((tile1 == player) && (tile2 == player) && (tile3 == player)) ||                            // 1
+    if (((tile1 == player) && (tile2 == player) && (tile3 == player)) || // 1
         (tile1 == player) && (tile4 == player) && (tile7 == player) ||
         (tile1 == player) && (tile5 == player) && (tile9 == player)) {
-            return true;
-    } else if (((tile2 == player) && (tile1 == player) && (tile3 == player)) ||                       // 2
+        return true;
+    } else if (((tile2 == player) && (tile1 == player) && (tile3 == player)) || // 2
         (tile2 == player) && (tile5 == player) && (tile8 == player)) {
-            return true;
-    } else if (((tile3 == player) && (tile1 == player) && (tile2 == player)) ||                       // 3
+        return true;
+    } else if (((tile3 == player) && (tile1 == player) && (tile2 == player)) || // 3
         (tile3 == player) && (tile6 == player) && (tile9 == player) ||
         (tile3 == player) && (tile5 == player) && (tile7 == player)) {
-            return true;
-    } else if (((tile4 == player) && (tile1 == player) && (tile7 == player)) ||                       // 4
+        return true;
+    } else if (((tile4 == player) && (tile1 == player) && (tile7 == player)) || // 4
         (tile4 == player) && (tile5 == player) && (tile6 == player)) {
-            return true;
-    } else if (((tile5 == player) && (tile2 == player) && (tile8 == player)) ||                      // 5 
+        return true;
+    } else if (((tile5 == player) && (tile2 == player) && (tile8 == player)) || // 5 
         (tile5 == player) && (tile4 == player) && (tile6 == player) ||
         (tile5 == player) && (tile3 == player) && (tile7 == player) ||
         (tile5 == player) && (tile1 == player) && (tile9 == player)) {
-            return true;
-    } else if (((tile6 == player) && (tile3 == player) && (tile9 == player)) ||                      // 6
+        return true;
+    } else if (((tile6 == player) && (tile3 == player) && (tile9 == player)) || // 6
         (tile6 == player) && (tile4 == player) && (tile5 == player)) {
-            return true;
-    } else if (((tile7 == player) && (tile1 == player) && (tile4 == player)) ||                     // 7 
+        return true;
+    } else if (((tile7 == player) && (tile1 == player) && (tile4 == player)) || // 7 
         (tile7 == player) && (tile5 == player) && (tile3 == player) ||
         (tile7 == player) && (tile8 == player) && (tile9 == player)) {
-            return true;
-    } else if (((tile8 == player) && (tile2 == player) && (tile5 == player)) ||                      // 8
+        return true;
+    } else if (((tile8 == player) && (tile2 == player) && (tile5 == player)) || // 8
         (tile8 == player) && (tile9 == player) && (tile7 == player)) {
-            return true;
-    } else if (((tile9 == player) && (tile6 == player) && (tile3 == player)) ||                       // 9
+        return true;
+    } else if (((tile9 == player) && (tile6 == player) && (tile3 == player)) || // 9
         (tile9 == player) && (tile8 == player) && (tile7 == player) ||
         (tile9 == player) && (tile5 == player) && (tile1 == player)) {
-            return true;
+        return true;
     }
 
     return false;
 }
 
-let LabX = document.querySelector('#player_X');
-console.log(LabX)
+
+
 let tileElements = document.getElementsByClassName('tile');
 for (var i = 0; i < tileElements.length; i++) {
     tileElements[i].addEventListener('click', function () {
-        if (finished==false) {
+        if (finished == false) {
             if (this.innerHTML == '') {
                 if (turn == 0) {
                     this.innerHTML = player_x;
-                    if(getEntry(player_x)){
+                    if (getEntry(player_x)) {
                         let alertWin = document.getElementById(`which-player-win`);
                         alertWin.innerHTML = "Winner is " + " " + player_x;
-                        Lab_X.innerHTML =+
+                        score_x += 1;
+                        let LabX = document.querySelector('#player_X');
+                        LabX.innerHTML = score_x;
                         finished = true;
                     }
                     turn = 1;
 
                 } else {
                     this.innerHTML = player_o;
-                    if(getEntry(player_o)){
+                    if (getEntry(player_o)) {
                         let alertWin = document.getElementById(`which-player-win`);
                         alertWin.innerHTML = "Winner is " + " " + player_o;
-
+                        score_o += 1;
+                        let LabO = document.querySelector('#player_O');
+                        LabO.innerHTML = score_o;
                         finished = true;
                     }
                     turn = 0;
@@ -88,15 +90,19 @@ for (var i = 0; i < tileElements.length; i++) {
         }
     });
 }
-let alertWin = document.getElementById(`which-player-win`);
-let start=document.getElementById('start');
 
-    start.addEventListener('click', () => {
-        for (let i = 0; i < tileElements.length; i++) {
-        tileElements[i].innerHTML=''
-        alertWin.innerHTML=''
-          }
-          finished=false;
-     });
-       
+start.addEventListener('click', () => {
+    main_board.style.display = "flex";
+    player_scores.style.display = "none"
+    let alertWin = document.getElementById(`which-player-win`);
+    alertWin.innerHTML = ''
+    for (let i = 0; i < tileElements.length; i++) {
+        tileElements[i].innerHTML = ''
+    }
+    finished = false;
+    turn = 0;
+});
 
+scores.addEventListener('click', () => {
+    player_scores.style.display = "block"
+});
